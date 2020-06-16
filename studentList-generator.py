965f5f86d412565cc57c85227ec8a18d68e9fb08
Thusithanjana Thilakarathana <50567237+Thusithanjana@users.noreply.github.com>
2020-06-16 07:02:10 +0530

    Added the comments

--- studentList-generator.py
+++ studentList-generator.py
@@ -4,6 +4,7 @@ import csv
 #print ([name for name in os.listdir(".") if os.path.isdir(name)])
 os.chdir('..')
 
+#This function will filter the it number from the folder name
 def findID(nameList):
     for i in nameList:
         #print(i)
@@ -11,16 +12,16 @@ def findID(nameList):
             return i
     
 
-
+#Write the data into the CSV file
 with open('NameList.csv', 'wb') as csvfile:
     theWriter = csv.writer(csvfile)
     theWriter.writerow(['StudentId'])
     for name in os.listdir("."):
         if os.path.isdir(name):
-            name = name.replace('_',' ')
+            name = name.replace('_',' ')#remove the '_' from the folder name
             if (name!='App'):
-                id = name.split(' ')        
-                theWriter.writerow([findID(id)])
+                id = name.split(' ')#Split the name         
+                theWriter.writerow([findID(id)])# this will filter the it number
                 
 
 
